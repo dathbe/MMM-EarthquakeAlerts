99c982ed266ef516617c0a718b412ca9a0ee9ebf
dathbe <github@beffa.us>
2025-04-03 02:38:11 -0700

    Add files via upload

--- MMM-EarthquakeAlerts.js
+++ MMM-EarthquakeAlerts.js
@@ -9,8 +9,16 @@ Module.register('MMM-EarthquakeAlerts', {
 
   // Default config.
   defaults: {
-    latitude: 39.1,
-    longitude: 94.6,
+    locations: [
+      { latitude: 39.1, longitude: -94.6, name: 'nowhere' },
+    ],
+    magnitude1: 2.5,
+    distance1: 10 * 1609,
+    magnitude2: 4.0,
+    distance2: 60 * 1609,
+    magnitude3: 5.5,
+    distance3: 300 * 1609,
+    magnitude4: 7.0,
     animationSpeed: 2 * 1000,
   },
 
@@ -22,7 +30,9 @@ Module.register('MMM-EarthquakeAlerts', {
   // Define start sequence.
   start() {
     this.notification = false
-    this.messageText = this.config.initialMessage
+    this.messageText = ''
+
+    this.sendSocketNotification('EARTHQUAKE_REQUEST', this.config)
   },
 
   // dom generator.
@@ -38,77 +48,15 @@ Module.register('MMM-EarthquakeAlerts', {
     return wrapper
   },
 
-  notificationReceived(notification, payload, sender) {
-    if (notification === 'EARTHQUAKEALERTS_UPDATE' && (payload.uniqueID == this.config.uniqueID || !this.config.uniqueID)) {
-      Log.debug(`Received notification: ${notification} with payload.message: ${payload.message} from sender: ${sender}`)
-      this.messageText = payload.message
+  // Deal with received notification
+  socketNotificationReceived(notification, payload) {
+    if (notification === 'EARTHQUAKE_ALERT') {
+      this.messageText = payload.quakeMessages.join('<br>')
       this.notification = true
       this.updateDom(this.config.animationSpeed)
     }
   },
 
+// NOW NEED TO HAVE IT REFRESH THE FEED PERIODICALLY
+
 })
-
-/*
-import requests
-import datetime
-import json
-import pandas as pd
-import geopy.distance
-import sys
-
-message = []
-
-### Add earthquakes
-today = datetime.datetime.now()
-yesterday = today - datetime.timedelta(days=1)
-quakeurl = f"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={yesterday.strftime('%Y-%m-%dT%H:%M')}-0800&minmagnitude=2.5"
-sunnysideloc = (33.7629, -118.3254)
-poppyloc = (33.7601,-116.3347)
-nploc = (33.7629, -118.3254)
-
-response = json.loads(requests.get(quakeurl).content)
-quakedata = pd.DataFrame(columns=["mag", "time", "place", "lat", "lon", "sunnysideDistance", "poppyDistance", "npDistance"])
-for quake in response['features']:
-	sunnysideDist = round(geopy.distance.geodesic(f"{quake['geometry']['coordinates'][1]}, {quake['geometry']['coordinates'][0]}", sunnysideloc).miles,0)
-	poppyDist = round(geopy.distance.geodesic(f"{quake['geometry']['coordinates'][1]}, {quake['geometry']['coordinates'][0]}", poppyloc).miles,0)
-	npDist = round(geopy.distance.geodesic(f"{quake['geometry']['coordinates'][1]}, {quake['geometry']['coordinates'][0]}", nploc).miles,0)
-	addit = False
-	if quake['properties']['mag'] >= 7.0:
-		addit = True
-	elif quake['properties']['mag'] >= 5.5 and (sunnysideDist <= 300 or poppyDist <= 300 or npDist <= 300):
-		addit = True
-	elif quake['properties']['mag'] >= 4.0 and (sunnysideDist <= 60 or poppyDist <= 60 or npDist <= 60):
-		addit = True
-	elif sunnysideDist <= 10 or poppyDist <= 10 or npDist <= 10:
-		addit = True
-	if addit or '-d' in sys.argv:
-		quakedata.loc[quake['id']] = [quake['properties']['mag'], quake['properties']['time'], quake['properties']['place'].split(" of ")[-1], quake['geometry']['coordinates'][1], quake['geometry']['coordinates'][0], sunnysideDist, poppyDist, npDist]
-
-if '-d' in sys.argv:
-	quakedata = quakedata.head(2)
-for i, quake in quakedata.iterrows():
-	hoursAgo = (today.timestamp() - quake['time']/1000)/60/60
-	quakeDescrip = f"{quake['mag']} earthquake {int(quake['sunnysideDistance'])} miles away "
-	if quake['poppyDistance'] < quake['sunnysideDistance'] and quake['poppyDistance'] <= quake['npDistance']:
-		quakeDescrip += f"({int(quake['poppyDistance'])} miles from Poppy) "
-	elif quake['npDistance'] < quake['sunnysideDistance']:
-		quakeDescrip += f"({int(quake['npDistance'])} miles from Normandy Park) "
-	quakeDescrip += f"near {quake['place']} {int(hoursAgo)} hours ago"
-	message += [quakeDescrip]
-
-### Combine and post message
-if len(message) > 0:
-	message = '<br>'.join(message)
-	message = f"{prefix}{message}{suffix}"
-else:
-	message = ''
-
-print(message)
-json = {
-	"uniqueID": "earthquakes",
-	"message": message
-}
-
-x = requests.post('http://192.168.1.107:8080/api/notification/CUSTOMTEXT_UPDATE', json=json)
-*/
--- node_helper.js
+++ node_helper.js
@@ -1,4 +1,7 @@
 const NodeHelper = require('node_helper')
+const moment = require('moment-timezone')
+// const pd = require('node-pandas')
+const geolib = require('geolib')
 
 module.exports = NodeHelper.create({
   start: function () {
@@ -6,16 +9,43 @@ module.exports = NodeHelper.create({
   },
 
   async getData(payload) {
+
+    // Fetch earthquake data for the last day (with at least magnitude1 magnitude)
+    var data = []
     try {
-      const url = 
+      const url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=' + moment().subtract(1, 'day').format() + '&minmagnitude=' + payload.magnitude1
       const response = await fetch(url)
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`)
       }
-      const data = await response.json()
-      this.sendSocketNotification("EARTHQUAKE_RESULTS", {
-        result: data
-      })
+      var quakes = await response.json()
+      quakes = quakes['features']
+
+      // Parse quake information
+      var quakeMessages = []
+      for (var quakeNo in quakes) {
+        var distances = []
+        for (var locNo in payload.locations) {
+          distances.push(
+            geolib.getPreciseDistance(
+              { latitude: payload.locations[locNo]['latitude'], longitude: payload.locations[locNo]['longitude'] },
+              { latitude: quakes[quakeNo]['geometry']['coordinates'][1], longitude: quakes[quakeNo]['geometry']['coordinates'][0] },
+            ),
+          )
+        }
+        var hoursAgo = (new Date() - quakes[quakeNo]['properties']['time']) / 1000 / 60 / 60
+        // if ((Math.min(distances) <= payload.distance1 && quake['properties']['mag'] >= payload.magnitude1)
+        //  || (Math.min(distances) <= payload.distance2 && quake['properties']['mag'] >= payload.magnitude2)
+        //  || (quake['properties']['mag'] >= payload.magnitude3)) {
+          quakeMessages.push(`${parseFloat(quakes[quakeNo]['properties']['mag']).toFixed(1)} earthquake ${Math.round(Math.min(...distances) / 1609)} miles from ${payload.locations[distances.indexOf(Math.min(...distances))]['name']} near ${quakes[quakeNo]['properties']['place'].split('of ')[1]} ${Math.floor(hoursAgo)} hours ago`)
+        // }
+      }
+      // Send message
+      if (quakeMessages.length > 0) {
+        this.sendSocketNotification('EARTHQUAKE_ALERT', {
+          quakeMessages: quakeMessages,
+        })
+      }
     }
     catch (error) {
       console.error('[MMM-EarthquakeAlerts] Could not load data.', error)
